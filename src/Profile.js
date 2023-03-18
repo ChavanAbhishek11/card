7db0ae8bdd140a1686e78dd4c71cdcfda2b568f3
Abhishek pandurang chavan <chavanabhishek724@gmail.com>
2023-03-18 22:43:44 +0530

    first commit

---
+++ src/Profile.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import image from './Abhishek photo5.jpeg'
+
+import "./Profile.css";
+
+const Profile = () => {
+  return (
+    <>
+    
+    <div className='container'>
+      <img src={image}  className="image"/>
+      <h1>Abhishek chavan</h1>
+      <h2>React Developer</h2>
+      <div className='SocialmediaLink'>
+      
+
+      </div>
+      </div>
+      <div className='container'>
+      <img src={image}  className="image"/>
+      <h1>Abhishek chavan</h1>
+      <h2>React Developer</h2>
+      <div className='SocialmediaLink'>
+      
+
+      </div>
+     </div>
+     <div className='container'>
+      <img src={image}  className="image"/>
+      <h1>Abhishek chavan</h1>
+      <h2>React Developer</h2>
+      <div className='SocialmediaLink'>
+      
+
+      </div>
+     </div>
+    
+    </>
+  )
+  
+}
+
+export default Profile
